47ecf7c1e6e46c0ba41678946100ec4f69b96d82
Jose Martinez <joselo.creativos@gmail.com>
2024-10-14 18:59:37 -0500

    Añadir archivos HTML, CSS y Javascript

---
+++ logica.js
@@ -0,0 +1,8 @@
+
+function comprar(){
+    window.open("https://www.siigo.com/precios-siigo/", "_blank");
+}
+
+function contacto(){
+    window.open("https://api.whatsapp.com/send?phone=573016321817&text=Hola%20M%C3%A1sterting,%20recib%C3%AD%20tu%20cotizaci%C3%B3n%20por%20email%20y%20estoy%20interesad@,%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre%20el%20servicio", "_blank");
+}
