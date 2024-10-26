913e6f4ecee122bd62380e1c9f976506319b3caf
Jose Martinez <joselo.creativos@gmail.com>
2024-10-26 13:41:59 -0500

    agregue desplegable menu iniciar sesión

--- logica.js
+++ logica.js
@@ -1,3 +1,25 @@
+let btonPriceCard = document.getElementById("btnDown");
+let btnLogin = document.getElementsByClassName("login")[0];
+let loginDesplegable = document.getElementsByClassName("loginDesplegable")[0];
+
+
+btnLogin.addEventListener("mouseover", mostrarMenu);
+btnLogin.addEventListener("mouseout", esconderMenu);
+loginDesplegable.addEventListener("mouseover", mostrarMenu);
+loginDesplegable.addEventListener("mouseout", esconderMenu);
+
+function mostrarMenu(){
+    loginDesplegable.classList.remove("inactive");
+}
+function esconderMenu(){
+    loginDesplegable.classList.add("inactive");
+}
+
+
+
+
+
+
 
 function comprar(){
     window.open("https://www.siigo.com/precios-siigo/", "_blank");
@@ -6,3 +28,4 @@ function comprar(){
 function contacto(){
     window.open("https://api.whatsapp.com/send?phone=573016321817&text=Hola%20M%C3%A1sterting,%20recib%C3%AD%20tu%20cotizaci%C3%B3n%20por%20email%20y%20estoy%20interesad@,%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre%20el%20servicio", "_blank");
 }
+
