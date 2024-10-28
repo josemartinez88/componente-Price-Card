f3ab39af160c3420da1a1674e140330396d03379
Jose Martinez <joselo.creativos@gmail.com>
2024-10-27 20:02:25 -0500

    agregamos desplegables del header productos, paises y sidepanel del menu hamburguesa en versión mobile

--- logica.js
+++ logica.js
@@ -1,12 +1,30 @@
 let btonPriceCard = document.getElementById("btnDown");
 let btnLogin = document.getElementsByClassName("login")[0];
 let loginDesplegable = document.getElementsByClassName("loginDesplegable")[0];
+let desplegable3 = document.getElementById("desplegable3");
+let paisDesplegable = document.getElementsByClassName("paisDesplegable")[0];
+let desplegable = document.getElementById("desplegable");
+let productosMenu = document.getElementsByClassName("productosMenu")[0];
+let menuMobile = document.getElementsByClassName("menuMobile")[0];
+let menuDesplegableMobile = document.getElementsByClassName("menuDesplegableMobile")[0];
+let cerrar = document.getElementById("cerrar");
+let overflow = document.getElementsByClassName("overflow-container")[0];
+
+
+
 
 
 btnLogin.addEventListener("mouseover", mostrarMenu);
 btnLogin.addEventListener("mouseout", esconderMenu);
 loginDesplegable.addEventListener("mouseover", mostrarMenu);
 loginDesplegable.addEventListener("mouseout", esconderMenu);
+desplegable3.addEventListener("mouseover", mostrarMenuPaises);
+desplegable3.addEventListener("mouseout", esconderMenuPaises);
+desplegable.addEventListener("mouseover", mostrarMenuProductos);
+desplegable.addEventListener("mouseout", esconderMenuProductos);
+menuMobile.addEventListener("click", mostrarMenuMobile);
+cerrar.addEventListener("click", esconderMenuMobile);
+
 
 function mostrarMenu(){
     loginDesplegable.classList.remove("inactive");
@@ -14,8 +32,26 @@ function mostrarMenu(){
 function esconderMenu(){
     loginDesplegable.classList.add("inactive");
 }
-
-
+function mostrarMenuPaises(){
+    paisDesplegable.classList.remove("inactive");
+}
+function esconderMenuPaises(){
+    paisDesplegable.classList.add("inactive");
+}
+function mostrarMenuProductos(){
+    productosMenu.classList.remove("inactive");
+}
+function esconderMenuProductos(){
+    productosMenu.classList.add("inactive");
+}
+function mostrarMenuMobile(){
+    menuDesplegableMobile.classList.remove("inactive");
+    overflow.classList.remove("inactive");
+}
+function esconderMenuMobile(){
+    menuDesplegableMobile.classList.add("inactive");
+    overflow.classList.add("inactive");
+}
 
 
 
