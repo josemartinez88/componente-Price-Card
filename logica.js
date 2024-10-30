e91215852f52c2ec92fbd566525b73e8920d2ec9
Jose Martinez <joselo.creativos@gmail.com>
2024-10-30 17:06:39 -0500

    arreglamos ancho de pricecards en mobile

--- logica.js
+++ logica.js
@@ -96,3 +96,14 @@ function contacto(){
     window.open("https://api.whatsapp.com/send?phone=573016321817&text=Hola%20M%C3%A1sterting,%20recib%C3%AD%20tu%20cotizaci%C3%B3n%20por%20email%20y%20estoy%20interesad@,%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre%20el%20servicio", "_blank");
 }
 
+
+
+
+
+
+
+
+
+
+
+
